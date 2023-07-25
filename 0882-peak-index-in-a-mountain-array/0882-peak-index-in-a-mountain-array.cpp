b25beacfea8109e4c2d80cb50fad731baba4dccc
Ankit Anjana <136179445+ankitanjana23@users.noreply.github.com>
2023-07-25 21:50:22 +0530

    Time: 125 ms (63.71%), Space: 59.5 MB (88.25%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        for(int i =0;i<arr.size();i++){
-            if(arr[i] > arr[i+1]) return i;
-        }
-        return 0;
+       return max_element(arr.begin() ,arr.end()) -arr.begin();
     }
 };
