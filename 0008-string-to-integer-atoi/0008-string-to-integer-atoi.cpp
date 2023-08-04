7e11063c0e36ee75776d35d91e2b559f3588326f
Ankit Anjana <136179445+ankitanjana23@users.noreply.github.com>
2023-08-04 16:43:56 +0530

    Time: 6 ms (27.93%), Space: 6.3 MB (100%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int i = 0;
-        int sign = 1;
-        int result = 0;
-        int n = s.length();
-
-        //white space 
-        while (i < n && s[i] == ' ') i++;
-
-        // Step 2: Check for '+' or '-'
-        if (i < n && (s[i] == '+' || s[i] == '-')) {
+        int n = s.size();
+        int sign =1;
+        int result =0 ;
+        //first leading white space remove 
+        int i =0;
+        while(i<n && s[i] == ' ') i++;
+        
+        //sign reach
+        if(i<n && s[i] == '-' || s[i] == '+'){
             sign = (s[i] == '-') ? -1 : 1;
             i++;
         }
 
-        // Step 3: Read in the digits
-        while (i < n && isdigit(s[i])) {
-            int digit = s[i] - '0';
-
-            // Step 5: Check for integer overflow
-            if (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10)) {
-                return (sign == -1) ? INT_MIN : INT_MAX;
+        // if digit arrival
+        while(i<n && isdigit(s[i])){
+            int digit  = s[i] -'0';
+             if (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > 7)) {
+                return (sign == 1) ? INT_MAX : INT_MIN;
             }
-
-            result = result * 10 + digit;
+            result = result*10 + digit;
             i++;
         }
-
-        return result * sign;
+        return result *sign ;
     }
 };
