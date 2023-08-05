f036bbd834d9559087ad7ac9a47ff3817dd3aef2
Ankit Anjana <136179445+ankitanjana23@users.noreply.github.com>
2023-08-05 16:02:21 +0530

    Time: 14 ms (85.3%), Space: 11.8 MB (99.23%) - LeetHub

---
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<TreeNode*> generateTrees(int n) {
+        vector<vector<vector<TreeNode*> > > dp(n + 1, vector(n + 1, vector<TreeNode*>(0)));
+        for (int i = 1; i <= n; i++) {
+            dp[i][i].push_back(new TreeNode(i));
+        }
+
+        for (int num = 2; num <= n; num++) {
+            for (int start = 1; start <= n - num + 1; start++) {
+                int end = start + num - 1;
+                for (int i = start; i <= end; i++) {
+                    vector<TreeNode*> leftSubtrees =
+                        i - 1 >= start ? dp[start][i - 1] : vector<TreeNode*>({NULL});
+                    vector<TreeNode*> rightSubtrees =
+                        i + 1 <= end ? dp[i + 1][end] : vector<TreeNode*>({NULL});
+
+                    for (auto left : leftSubtrees) {
+                        for (auto right : rightSubtrees) {
+                            TreeNode* root = new TreeNode(i, left, right);
+                            dp[start][end].push_back(root);
+                        }
+                    }
+                }
+            }
+        }
+        return dp[1][n];
+    }
+};
