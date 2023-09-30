2e1f8b7ef6e561d84bf5e754611c1b9011002510
Ankit Anjana <136179445+ankitanjana23@users.noreply.github.com>
2023-10-01 05:28:21 +0530

    Time: 85 ms (45.53%), Space: 68 MB (26.8%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,21 +1,19 @@
-#include <bits/stdc++.h>
+
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-       /* My solution */
-      int n = nums.size();
-      int cnt =0;
-      int maxi = INT_MIN;
-      if(n==1) {
-          return nums[0];
-      }
-      for(int i =0;i<n;i++){
-        //    int new1 =0;
-        //    new1 = nums[i]
-           cnt +=nums[i];
-           maxi = max(cnt,maxi);
-           if(cnt<0) cnt =0;
-      }
-      return maxi;
+       // Divide and Conquer method to solve this 
+       int cmax = 0;
+       int omax = INT_MIN;
+       for(int i =0;i<nums.size() ;i++){
+           if(cmax<0) {
+               cmax = nums[i] ;
+           }
+           else {
+               cmax += nums[i];
+           }
+           omax = max(omax, cmax);
+       }
+       return omax;
     }
 };
